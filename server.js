1e8040d18a1cc5488737fdb7ce991770800192a6
Korell0 <korsos.adam97@gmail.com>
2022-09-09 10:26:22 +0200

    teszt sikeres

--- server.js
+++ server.js
@@ -1,12 +1,22 @@
-
 const express  = require('express');
-const app = express;
-const port = 1916;
+const app = express();
+const port = 5000;
 const path = require('path');
 
 app.use('/css',express.static(path.join(__dirname+"/css")));
-app.get('/',express.static(path.join(__dirname+"/index.html")));
-app.get('/hobbies',express.static(path.join(__dirname+"/hobbies.html")));
-app.get('/contacts',express.static(path.join(__dirname+"/contacts.html")));
 
+app.get('/', (req, res)=>{
+    res.status(200).sendFile(path.join(__dirname+"/index.html"))
+});
 
+app.get('/hobbies', (req, res)=>{
+    res.status(200).sendFile(path.join(__dirname+"/hobbies.html"))
+});
+
+app.get('/contacts', (req, res)=>{
+    res.status(200).sendFile(path.join(__dirname+"/contacts.html"))
+});
+
+app.listen(port, ()=>{
+    console.log(`Server listening ON ${port} port...`);
+})
